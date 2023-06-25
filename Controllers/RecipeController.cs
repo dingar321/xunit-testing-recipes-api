01976e36550f362412358946de192cdab72d734b
Dino Garić <dinogaric1@gmail.com>
2023-06-25 12:51:42 +0100

    Feature create new recipe
    Also fixed the dto with added required data annotations

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipes.Api.Models;
+using Recipes.Api.Service;
+
+namespace Recipes.Api.Controllers;
+
+[ApiController]
+[Route("recipes")] 
+public class RecipeController : ControllerBase
+{
+    private readonly IRecipeService _recipeService;
+
+    public RecipeController(IRecipeService recipeService)
+    {
+        _recipeService = recipeService;
+    }
+
+    /// <summary>
+    /// Creates a new recipe
+    /// </summary>
+    /// <response code="201"> Created a new recipe </response>
+    /// <response code="400"> The body for a new recipe was not valid</response>
+    [HttpPost]
+    [Route("create")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateRecipe(
+        [FromBody] RecipeDto newRecipe)
+    {
+        await _recipeService.CreateRecipe(
+            new Recipe
+            {
+                RecipeName = newRecipe.RecipeName,
+                Description = newRecipe.Description,
+                Ingredients = newRecipe.Ingredients,
+                Instructions = newRecipe.Instructions
+            });
+
+        return Ok(); 
+    }
+}
--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -13,8 +13,8 @@ public class Recipe
 
 public class RecipeDto
 {
-    public required string RecipeName { get; set; }
-    public required string Description { get; set; }
-    public required string Ingredients { get; set; }
-    public required string Instructions { get; set; }
+    [Required] public required string RecipeName { get; set; }
+    [Required] public required string Description { get; set; }
+    [Required] public required string Ingredients { get; set; }
+    [Required] public required string Instructions { get; set; }
 }
